54c15332a417e805594b8916b95efd4e0dcc76d7
Yuki N <lunarbase@outlook.com>
2018-10-28 22:04:46 +0000

    automatically backed up by learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,10 @@
 #write your code here
 
-def countdown
+def countdown(number)
+  count = 0  
+  while count > -1
+    puts "#{count} SECOND(S)"
+    count -= 1
+  end
+    puts "HAPPY NEW YEAR!"
 end
